9fdfcf1d1c7da8bf917a9c8a6fc07ce9d4918a86
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-17 19:24:56 +0100

    Modify Layout and Style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
+import mkLogo from './assets/mk.png';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'thefemiayodeji';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
@@ -11,23 +12,25 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+          <p className="header gradient-text">
+            Meta 
+            <img className="mk-logo" src={mkLogo} alt='MK Logo'/>
+            Kombat
+          </p>
           <p className="sub-text">Team up to protect the Metaverse!</p>
           <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
-            />
           </div>
         </div>
         <div className="footer-container">
+          <span className="footer-text">Built by </span>
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
             className="footer-text"
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
+          >{`@${TWITTER_HANDLE}`}</a>
+          <span  className="footer-text">All Rights Licensed</span>
         </div>
       </div>
     </div>
