d3f8caf6242a63eb1a9da8f64456969899882358
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-18 18:57:23 +0100

    Render Player Character

--- src/Components/Arena/index.js
+++ src/Components/Arena/index.js
@@ -64,7 +64,31 @@ const Arena = ({ characterNFT }) => {
                 </div>
             </div>
         )}
-      <p>CHARACTER NFT GOES HERE</p>
+
+        {characterNFT && (
+            <div className="players-container">
+                <div className="player-container">
+                <p className='player-text'>Your Character</p>
+                <div className="player">
+                    <div className="image-content">
+                        <h2>{characterNFT.name}</h2>
+                        <img
+                        src={characterNFT.imageURI}
+                        alt={`Character ${characterNFT.name}`}
+                        />
+                        <div className="health-bar">
+                        <progress value={characterNFT.hp} max={characterNFT.maxHp} />
+                        <p>{`${characterNFT.hp} / ${characterNFT.maxHp} HP`}</p>
+                        </div>
+                    </div>
+                    <div className="stats">
+                        <h4>{`⚔️ Attack Damage: ${characterNFT.attackDamage}`}</h4>
+                    </div>
+                </div>
+                </div>
+            </div>
+        )}
+
     </div>
   );
 };
